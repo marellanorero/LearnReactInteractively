22204d8bd138abaa0f9950061a855773c294dc6b
marellanorero <marellanonorero@gmail.com>
2022-05-24 15:58:01 +0000

    commit

--- exercises/03.2-a-real-component/app.jsx
+++ exercises/03.2-a-real-component/app.jsx
@@ -2,6 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //create your function here
+export const BootstrapCard = () => {
+	<div class="card m-5">
+		<img class="card-img-top" src="../../.learn/assets/Dylan.png?raw=true" alt="Card image cap" />
+		<div class="card-body">
+			<h5 class="card-title">Bob Dylan</h5>
+			<p class="card-text">
+				Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an influential
+				figure in popular music and culture for more than five decades.
+			</p>
+			<a href="https://en.wikipedia.org/wiki/Bob_Dylan" class="btn btn-primary">
+				Go to wikipedia
+			</a>
+		</div>
+	</div>;
+};
 
 //remember to use ReactDOM.render to include the component into the website
-
+ReactDOM.render(<BootstrapCard />, document.querySelector("#myDiv"));
