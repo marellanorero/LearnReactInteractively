c30b4cc621fb18636c3131bb8f8c9abd43cc0714
marellanorero <marellanonorero@gmail.com>
2022-05-25 23:11:23 +0000

    finalizado

--- .learn/resets/05.1-second-styles/app.jsx
+++ .learn/resets/05.1-second-styles/app.jsx
@@ -4,23 +4,15 @@ import PropTypes from "prop-types";
 
 const buttonStyles = {
 	//write button styles here
-	backgroundColor: "yellow",
-	fontSize: "16px",
-	border: "none",
-	color: "black",
+
 };
 
 const badgeStyles = {
 	//write the span styles here
-	backgroundColor: "red",
-	borderRadius: "50%",
-	padding: "3px",
-	margin: "3px",
-	color: "white",
-	fontSize: "16px",
+
 };
 
-const Badge = (props) => {
+const Badge = props => {
 	return (
 		<button style={buttonStyles} type="button" className="btn btn-primary">
 			{props.label}
@@ -32,7 +24,7 @@ const Badge = (props) => {
 };
 Badge.propTypes = {
 	label: PropTypes.string,
-	number: PropTypes.string,
+	number: PropTypes.string
 };
 //dont forget to change the label
 ReactDOM.render(<Badge label="Alerts" number="2" />, document.querySelector("#myDiv"));
--- exercises/04.1-conditional-rendering/app.jsx
+++ exercises/04.1-conditional-rendering/app.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
-const Alert = props => {
-	//your component here
+// add the green inside this function
+const Alert = (props) => {
+	const colorClasses = {
+		red: "alert-danger",
+		orange: "alert-warning",
+	};
+	if (colorClasses[props.color] === undefined) alert(`The color ${props.color} is not in the possible list of colors`);
 
+	return (
+		<div className={`alert ${colorClasses[props.color]}`} role="alert">
+			{props.text}
+		</div>
+	);
 };
 Alert.propTypes = {
 	color: PropTypes.string,
-	text: PropTypes.string
+	text: PropTypes.string,
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
-// but it helps you understand what properties are being passed to the component
+// but it helps you understand what properties is the component using
 ReactDOM.render(
 	<div>
 		<Alert text="OMG! Something really bad has happended!" color="red" />
